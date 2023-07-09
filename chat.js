6d26feabe3701de368e1e30c8f607db304cc02a2
kodrr <ntlauigan@gmail.com>
2023-07-09 22:10:12 +0800

    remove question

--- chat.js
+++ chat.js
@@ -11,7 +11,7 @@ require("dotenv").config();
   const history = [];
 
   while (true) {
-    const user_input = readlineSync.question("Your input: ");
+    const user_input = readlineSync.question("\nYour input: ");
 
     const messages = [];
     for (const [input_text, completion_text] of history) {
@@ -28,19 +28,19 @@ require("dotenv").config();
       });
 
       const completion_text = completion.data.choices[0].message.content;
-      console.log(completion_text);
+      console.log("\nBeshyko:", completion_text);
 
       history.push([user_input, completion_text]);
 
-      const user_input_again = readlineSync.question(
-        "\nWould you like to continue the conversation? (Y/N)"
-      );
-      if (user_input_again.toUpperCase() === "N") {
-        return;
-      } else if (user_input_again.toUpperCase() !== "Y") {
-        console.log("Invalid input. Please enter 'Y' or 'N'.");
-        return;
-      }
+      // const user_input_again = readlineSync.question(
+      //   "\nWould you like to continue the conversation? (Y/N)"
+      // );
+      // if (user_input_again.toUpperCase() === "N") {
+      //   return;
+      // } else if (user_input_again.toUpperCase() !== "Y") {
+      //   console.log("Invalid input. Please enter 'Y' or 'N'.");
+      //   return;
+      // }
     } catch (error) {
       if (error.response) {
         console.log(error.response.status);
